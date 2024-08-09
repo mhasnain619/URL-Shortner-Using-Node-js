c866feec642afc00f05fc6d912f8fec147a4bdf4
Muhammad Hasnain <hasnain0893@gmail.com>
2024-08-09 15:59:30 +0500

    working on it

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express")
+const { connectToMongoDB } = require('./connect')
+
+const urlRoute = require('./routes/url')
+
 
 const app = express()
 const PORT = 8001
 
+connectToMongoDB('mongodb://localhost:27017/short-url')
+    .then(() => {
+        console.log('Mongodb Connected Successfully');
+
+    })
+
+app.use(express.json())
+app.use('/', urlRoute)
 
 app.listen(PORT, () => {
-    console.log("Server Started at PORT" + PORT);
+    console.log("Server Started at PORT : " + PORT);
 })
