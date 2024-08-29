8e158a446af53b639cb36626c19570d734271291
Muhammad Hasnain <hasnain0893@gmail.com>
2024-08-29 15:49:18 +0500

    authorization

--- controllers/user.js
+++ controllers/user.js
@@ -22,7 +22,7 @@ async function handleUserLogin(req, res) {
         })
     }
     const token = setUser(user)
-    res.cookie('uid', token)
-    return res.redirect('/')
+    // res.cookie('uid', token)
+    return res.json({ token })
 }
 module.exports = { handleUserSignup, handleUserLogin }
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,33 +2,35 @@ const { getUser } = require('../services/auth');
 
 async function restrictToLogedUserOnly(req, res, next) {
     try {
-        const userUid = req.cookies?.uid;
-        console.log('User UID:', userUid);
+        const userUid = req.headers['authorization'];
 
         if (!userUid) {
-            console.log('No UID found in cookies, redirecting to login.');
             return res.redirect('/login');
         }
 
-        const user = await getUser(userUid);
+        const token = userUid.split('Bearer ')[1]
+
+        const user = await getUser(token);
         // console.log('Fetched User:', user);
 
         if (!user) {
-            console.log('No user found, redirecting to login.');
+            // console.log('No user found, redirecting to login.');
             return res.redirect('/login');
         }
 
         req.user = user;
         return next();
     } catch (error) {
-        console.error('Error fetching user:', error);
+        // console.error('Error fetching user:', error);
         return res.redirect('/login');
     }
 }
 async function checkAuth(req, res, next) {
     try {
-        const userUid = req.cookies?.uid;
-        const user = await getUser(userUid);
+        const userUid = req.headers['authorization'];
+        const token = userUid.split('Bearer ')[1]
+
+        const user = await getUser(token);
         req.user = user;
         return next();
     } catch (error) {
